da0b0c3eb74dd3c5eddebb399046f7a22594ef1c
Tu Nombre <nahuelrabeywork@gmail.com>
2026-06-23 23:26:29 -0300

    fix vercel deploy: adapter-static + outputDirectory

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel'
+import adapter from '@sveltejs/adapter-static'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import { mdsvex } from 'mdsvex'
 
@@ -7,7 +7,7 @@ const config = {
 	preprocess: [vitePreprocess(), mdsvex({ extensions: ['.md'] })],
 	extensions: ['.svelte', '.md'],
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({ fallback: '404.html' }),
 		prerender: {
 			handleHttpError: 'warn',
 			handleUnseenRoutes: 'warn'
